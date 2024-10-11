5a14984d5b667c097b8148166b6089753bcbe92b
Magpiny <magpinyb@gmail.com>
2024-10-12 01:14:22 +0300

    Done polymorphism

--- classes/inheritance.cpp
+++ classes/inheritance.cpp
@@ -1,11 +1,11 @@
 #include <print>
 #include <string>
 
-using std::string;
 using std::println;
+using std::string;
 
 class Animal {
-virtual void animal_details() = 0;
+  virtual void animal_details() = 0;
 
 protected:
   int age;
@@ -16,12 +16,8 @@ public:
   Animal(int Age, string colour, string Name)
       : age(Age), color(colour), name(Name) {};
 
-    void setSize(int Age){
-        age = Age;
-    }
-    int getAge(){
-        return age;
-    }
+  void setSize(int Age) { age = Age; }
+  int getAge() { return age; }
 
   virtual ~Animal() {};
 };
@@ -36,24 +32,22 @@ public:
   CAT(int age, string color, string name, int Legs, string Breed)
       : Animal(age, color, name), legs(Legs), breed(Breed) {};
 
-    void animal_details() override{
-        println("Animal Name: {}", name);
-        println("Animal Age: {}yrs old", age);
-        println("Animal Colour: {}", color);
-        println("Animal Breed: {}", breed);
-        println("Animal Number of Legs: {}", legs);
-        println("");
-    }
-
+  void animal_details() override {
+    println("Animal Name: {}", name);
+    println("Animal Age: {}yrs old", age);
+    println("Animal Colour: {}", color);
+    println("Animal Breed: {}", breed);
+    println("Animal Number of Legs: {}", legs);
+    println("");
+  }
 };
 
 int main() {
-    CAT mycat(3,"white","Cute Bunny", 4, "Brazillian");
-    CAT kitten = CAT(1, "grey", "Cute Sussy", 4, "Mexican");
+  CAT mycat(3, "white", "Cute Bunny", 4, "Brazillian");
+  CAT kitten = CAT(1, "grey", "Cute Sussy", 4, "Mexican");
 
-    // Display CATs details
-    mycat.animal_details();
-    kitten.animal_details();
-    println("Kitten is {} years old", kitten.getAge());
+  // Display CATs details
+  mycat.animal_details();
+  kitten.animal_details();
+  println("Kitten is {} years old", kitten.getAge());
 }
-
---
+++ classes/polymorphism.cpp
@@ -0,0 +1,50 @@
+#include <print>
+#include <string>
+
+using namespace std;
+
+class Fish {
+protected:
+  string name;
+
+  virtual void fish_details() = 0;
+
+public:
+  string type;
+  int weight;
+
+  Fish(string n, string t, int w) : name(n), type(t), weight(w) {};
+
+  void set_name(string name) { name = name; }
+
+  string get_name() { return name; }
+
+  virtual ~Fish() {};
+};
+
+class Tilapia : public Fish {
+public:
+  string size;
+
+  Tilapia(string nm, string typ, int weight, string size)
+      : Fish(nm, typ, weight), size(size) {};
+
+  void fish_details() override {
+    // polymorphism in action:
+    println("Fish Name: {}", name);
+    println("Fish Type: {}", type);
+    println("Fish Weight: {}", weight);
+    println("Fish Size: {}", size);
+  }
+};
+
+void getFishName(Tilapia &tilap) {
+  println("My Fish Name: {}", tilap.get_name());
+}
+
+int main() {
+  Tilapia tilap = Tilapia("Ngege", "lake", 4, "medium");
+  getFishName(tilap);
+  println("Fish Two {}", tilap.get_name());
+  tilap.fish_details();
+}

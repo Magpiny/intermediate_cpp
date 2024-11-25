6b19ff5e8208fd5eae1483b0abaaf8f003160492
Magpiny <magpinyb@gmail.com>
2024-11-25 23:56:53 +0300

    Learnt vector iteration

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -15,7 +15,26 @@ typedef std::vector<Student> vec;
 int
 main()
 {
-  vec stud1 = { Student("Sam", 23, "4 Blue") };
+  std::vector<int> numbers{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  std::vector<str> fruits = { "mangoe", "guava", "apple", "avocado" };
 
-  std::println("Vector test: 123", );
+  vec stud1 = { { "Sam", 24, "4 Blue" },   { "John", 21, "4 Green" },
+                { "Mathew", 23, "4 Red" }, { "Mike", 20, "4 Red" },
+                { "Paul", 22, "4 Green" }, { "Horace", 24, "4 Blue" } };
+
+  // Add an element to a vector
+  stud1.push_back({ "Luke", 22, "Green" });
+
+  // Filter out students from red class/stream
+
+  for (const auto& student : stud1) {
+    if (student.mclass == "4 Red")
+      std::println("Name: {} Age: {} Class: {}",
+                   student.name,
+                   student.age,
+                   student.mclass);
+  }
+
+  std::println("Vector test: {}", stud1.at(1).name);
+  std::println("Number of students: {}", stud1.size());
 }

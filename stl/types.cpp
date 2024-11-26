91a550f56e0f1cd82e8b014d5744d0237dd6ab85
Magpiny <magpinyb@gmail.com>
2024-11-26 20:00:30 +0300

    Learnt vector iteration

---
+++ stl/types.cpp
@@ -0,0 +1,9 @@
+#include <cstddef>
+#include <print>
+
+int
+main()
+{
+  std::size_t time;
+  std::println("Size of size_t is {}", sizeof(size_t));
+}
--- stl/vector.cpp
+++ stl/vector.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cmath>
 #include <print>
 #include <string>
 #include <vector>
@@ -18,12 +20,22 @@ main()
   std::vector<int> numbers{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
   std::vector<str> fruits = { "mangoe", "guava", "apple", "avocado" };
 
+  // Iterating over vector elements v1
+  std::for_each(numbers.begin(), numbers.end(), [](int& number) {
+    std::println("square of {} is {}", number, std::pow(number, 2));
+  });
+
+  // Iterating over vector elements v2
+  for (auto fruit = fruits.begin(); fruit != fruits.end(); fruit++) {
+    std::println("Fruit name: {}", *fruit);
+  }
+
   vec stud1 = { { "Sam", 24, "4 Blue" },   { "John", 21, "4 Green" },
                 { "Mathew", 23, "4 Red" }, { "Mike", 20, "4 Red" },
                 { "Paul", 22, "4 Green" }, { "Horace", 24, "4 Blue" } };
 
   // Add an element to a vector
-  stud1.push_back({ "Luke", 22, "Green" });
+  stud1.push_back({ "Luke", 22, "4 Red" });
 
   // Filter out students from red class/stream
 
